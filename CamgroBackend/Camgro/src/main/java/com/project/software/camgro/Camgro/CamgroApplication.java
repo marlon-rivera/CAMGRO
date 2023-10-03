881ac09a320a6b0ba666caeb545be1f967f46198
marlon-rivera <marlon.rivera@uptc.edu.co>
2023-10-03 17:11:23 -0500

    Base proyecto

---
+++ CamgroBackend/Camgro/src/main/java/com/project/software/camgro/Camgro/CamgroApplication.java
@@ -0,0 +1,13 @@
+package com.project.software.camgro.Camgro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamgroApplication.class, args);
+	}
+
+}
---
+++ CamgroBackend/Camgro/src/test/java/com/project/software/camgro/Camgro/CamgroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.software.camgro.Camgro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamgroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
