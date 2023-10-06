d6dfb297a4d0521b9ac4f6898fc156f90ccd0ff5
Sergio-711 <sergio.valcarcel@uptc.edu.co>
2023-10-06 17:05:40 -0500

    Se creó la clase Image

--- CamgroBackend/Camgro/src/main/java/com/project/software/camgro/Camgro/domain/Image.java
+++ CamgroBackend/Camgro/src/main/java/com/project/software/camgro/Camgro/domain/Image.java
@@ -1,15 +1,30 @@
 package com.project.software.camgro.Camgro.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="imagenes")
 public class Image {
 
     @Id
-    
+    @Column(name="id_iamgenes")
+    private String idImage;
+    @ManyToOne
+    @JoinColumn(name="id_publicacion")
+    private Post post;
+    @Column(name="url_image")
+    private String url;
+    @Column(name="nombre_image")
+    private String nameImage;
+    @Column(name="fecha_carga")
+    private LocalDate uploadDate;
+
 }
