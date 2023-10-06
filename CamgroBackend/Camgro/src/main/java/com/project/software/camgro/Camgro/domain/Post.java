b488d3436f17fe6c31099300805a16496bc56f4c
Sergio-711 <sergio.valcarcel@uptc.edu.co>
2023-10-06 16:48:20 -0500

    Se creó la clase Post en el BackEnd

---
+++ CamgroBackend/Camgro/src/main/java/com/project/software/camgro/Camgro/domain/Post.java
@@ -0,0 +1,38 @@
+package com.project.software.camgro.Camgro.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Table
+@Entity(name="publicaciones")
+@Data
+public class Post {
+
+    @Id
+    @Column(name="id_publicacion")
+    private String idPost;
+    @ManyToOne
+    private Account account;
+    @ManyToOne
+    private Place place;
+    @Column(name="precio_productos")
+    private double priceProduct;
+    @Column(name="cantidad_productos")
+    private int amountProducts;
+    @Column(name="titulo_publicacion")
+    private String postTitle;
+    @Column(name="descipcion_publicacion")
+    private String descriptionPost;
+    @Column(name="fecha_publicacion")
+    private LocalDate publicationDate;
+    @Column(name="fecha_cosecha")
+    private LocalDate harvestDate;
+    @Column(name="unidad_medida")
+    private String measureUnit;
+    @Column(name="estado_publicacion")
+    private String postStatus;
+
+
+}
