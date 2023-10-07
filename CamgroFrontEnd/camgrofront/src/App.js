42f8011db0542c1976c5c6ad240d83247b4c7485
Sergio-711 <sergio.valcarcel@uptc.edu.co>
2023-10-07 00:03:01 -0500

    No sé solo es para verificar que haya subido todo

--- CamgroFrontEnd/camgrofront/src/App.js
+++ CamgroFrontEnd/camgrofront/src/App.js
@@ -5,7 +5,7 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<Button src='./Static/hogar.png' text='prueba' width='30px' />
+				<Button src='./Static/perro.jpg' text='prueba' width='30px' />
 			</header>
 		</div>
 	);
--- CamgroFrontEnd/camgrofront/src/Components/Button.js
+++ CamgroFrontEnd/camgrofront/src/Components/Button.js
@@ -1,10 +1,18 @@
+import PropTypes from "prop-types";
+
 function Button(props) {
 	return (
 		<button>
-			<img src={this.props.src} width={this.props.width} />
-			<span>{this.props.text}</span>
+			<img src={props.src} width={props.width} />
+			<span>{props.text}</span>
 		</button>
 	);
+}
+
+Button.propTypes = {
+	src: PropTypes.string,
+	text: PropTypes.string,
+	width: PropTypes.string
 }
 
 export default Button;
