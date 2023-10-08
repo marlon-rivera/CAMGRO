7af8160f6285149cfc6bf6e3dc12b8ebe1b600c8
Sergio-711 <sergio.valcarcel@uptc.edu.co>
2023-10-07 22:06:48 -0500

    Se hizo lo del react-router

--- CamgroFrontEnd/camgrofront/src/Components/App.jsx
+++ CamgroFrontEnd/camgrofront/src/Components/App.jsx
@@ -1,10 +1,8 @@
-import Login from './Login'
+import MainRouter from '../Routers/MainRouter'
 
 function App() {
 	return (
-		<div className='App'>
-			<Login />
-		</div>
+		<MainRouter />
 	);
 }
 
---
+++ CamgroFrontEnd/camgrofront/src/Routers/MainRouter.js
@@ -0,0 +1,20 @@
+import { Routes, Route, BrowserRouter} from 'react-router-dom'
+import Login from '../Components/Login'
+import Register from '../Components/Register'
+
+function MainRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/registrar' element={<Register/>}/>
+        <Route path='*' element={
+        <>
+          <h1>Error 4040</h1> 
+          <strong>Esta página no existe</strong>
+        </>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+} 
+export default MainRouter;
