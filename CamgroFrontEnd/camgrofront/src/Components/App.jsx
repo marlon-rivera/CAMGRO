832c2aaeb29139b1e3f4f4c3fa453dbe99637fa3
= <=>
2023-10-07 22:18:02 -0500

    errores corregidos

--- CamgroFrontEnd/camgrofront/src/Components/App.jsx
+++ CamgroFrontEnd/camgrofront/src/Components/App.jsx
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-import Register from './Register'
-import Header from './Header'
-
-function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<Register />
-		</div>
-=======
 import MainRouter from '../Routers/MainRouter'
 
 function App() {
 	return (
 		<MainRouter />
->>>>>>> 7af8160f6285149cfc6bf6e3dc12b8ebe1b600c8
 	);
 }
 
