1c2c38a128044c74d1a5e933d1606d1dd94e0832
Sergio-711 <sergio.valcarcel@uptc.edu.co>
2023-10-19 08:47:29 -0500

    react-redux en desarrollo

---
+++ CamgroFrontEnd/camgrofront/src/redux/actions/actionsCreators.js
@@ -0,0 +1,11 @@
+export const UPDATE_TOKEN = 'UPDATE_TOKEN'
+export const GET_INFO_ACCOUNT = 'GET_INFO_ACCOUNT'
+const API_REST = 'https://localhost:8080'
+
+export const updateToken = (token) => (distpach) => {
+
+}
+
+export const getInfoPerson = (id) => (distpach) => {
+    fetch(API_REST)
+}
